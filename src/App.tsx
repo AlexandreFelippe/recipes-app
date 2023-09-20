eaec2f731a6f052f31d9c084fa0465d1e84bdd93
RianFelixx <rfo@etepd.com>
2023-09-20 14:23:46 -0300

    Requisitos 2 e 3 - Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <>
+      <div className="meals">
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+      </div>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+      </Routes>
+
+    </>
   );
 }
 
