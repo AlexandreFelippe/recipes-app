11f3e72b374dde18260a2bfc32fc42a8e2430fd4
AlexandreFelippe <alemfelippe@gmail.com>
2023-09-20 18:18:32 -0300

    resolve merge

---
+++ src/pages/FavoriteRecipies.tsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function FavoriteRecipies() {
+  return (
+    <>
+      <Header title="Favorite Recipes" search={ false } profile />
+      <div>FavoriteRecipies</div>
+    </>
+  );
+}
