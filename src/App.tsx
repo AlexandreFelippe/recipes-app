414bf8fe4105295d53b637c8aa9789a7cb16a6ec
Felipe Moreira <felipepenha01@gmail.com>
2023-09-20 15:47:45 -0400

    requisito 4 feito e lint ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-      <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
-      </div>
-      <Routes>
-        <Route path="/" element={ <Login /> } />
-      </Routes>
-
-    </>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+    </Routes>
   );
 }
 
