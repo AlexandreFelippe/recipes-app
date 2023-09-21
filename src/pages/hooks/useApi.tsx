305da22d5e5a1df9f3ac718c9b557397affd93a8
DaniloRNakasawa <danilonakasawa@outlook.com>
2023-09-21 14:54:36 -0300

    userApi

--- src/pages/hooks/useApi.tsx
+++ src/pages/hooks/useApi.tsx
@@ -8,21 +8,27 @@ export function useApi() {
   const searchIngredient = useCallback(async (ingrediente: string) => {
     setSearch('ingrediente');
     if (ingrediente.trim() !== '') {
-      const results = await fetchIngredients(ingrediente);
-      setSearchResult(results);
+      const result = await fetchIngredients(ingrediente);
+      setSearchResult(result);
     }
   }, []);
 
   const searchName = useCallback(async (name: string) => {
     setSearch('name');
     if (name.trim() !== '') {
-      return fetchName(name);
+      const resultName = await fetchName(name);
+      setSearchResult(resultName);
     }
   }, []);
 
   const searchFirstLetter = useCallback(async (letter: string) => {
+    setSearch('letter');
+    if (letter.trim() !== '') {
+      const resultLeter = await fetchFirstLetter(letter);
+      setSearchResult(resultLeter);
+    }
     return fetchFirstLetter(letter);
   }, []);
 
-  return { searchIngredient, searchName, searchFirstLetter };
+  return { searchType, searchResult, searchIngredient, searchName, searchFirstLetter };
 }
