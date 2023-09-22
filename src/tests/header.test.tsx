303f9edeb6c5447e3168283ba68a3bf6bb1f4f82
AlexandreFelippe <alemfelippe@gmail.com>
2023-09-22 20:09:30 -0300

    altera teste

--- src/tests/header.test.tsx
+++ src/tests/header.test.tsx
@@ -1,6 +1,7 @@
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderwithRouter';
+import renderWithRouterAndRedux from '../utils/renderWithRouterAndRedux';
 import Meals from '../pages/Meals';
 import Drinks from '../pages/Drinks';
 import DoneRecipes from '../pages/DoneRecipes';
@@ -8,7 +9,7 @@ import FavoriteRecipes from '../pages/FavoriteRecipes';
 import App from '../App';
 
 test('Testa a rota Meals:', () => {
-  renderWithRouter(<Meals />);
+  renderWithRouterAndRedux(<Meals />);
   const searchImage = screen.getByRole('img', { name: /pesquisar/i });
   const profileButton = screen.getByRole('button', { name: /perfil/i });
   const h1 = screen.getByRole('heading', { name: /meals/i });
