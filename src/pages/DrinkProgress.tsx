c040084c2a2e6319034c364f6d5008474773f746
RianFelixx <rfo@etepd.com>
2023-09-27 23:05:59 -0300

    Requisito 37

--- src/pages/DrinkProgress.tsx
+++ src/pages/DrinkProgress.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchDrinksRecipesDetails } from '../utils/SearchApi';
 
 export default function DrinkProgress() {
+  const [drinks, setDrinks] = useState<any>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!id) return;
+    const fechtsApi = async () => {
+      try {
+        const dataDrinks = await fetchDrinksRecipesDetails(id);
+        if (dataDrinks) setDrinks(dataDrinks);
+      } catch (error) {
+        console.error('Erro ao buscar detalhes da bebida:', error);
+      }
+    };
+    fechtsApi();
+  }, [id]);
+
+  const getIngredients = (drink: any) => {
+    const validIngredients: string[] = [];
+    for (let index = 1; index <= 20; index++) {
+      const ingredientKey = `strIngredient${index}`;
+      const measureKey = `strMeasure${index}`;
+      const ingredientValue = drink[ingredientKey];
+      const measureValue = drink[measureKey];
+      if (ingredientValue && ingredientValue !== null) {
+        const combinedValue = `${measureValue} ${ingredientValue}`;
+        validIngredients.push(combinedValue);
+      }
+    }
+    return validIngredients;
+  };
+
   return (
     <div>
-      DrinkProgress
+      {Array.isArray(drinks) && drinks.map((drink: any, drinkIndex: any) => (
+        <div key={ drinkIndex }>
+          <button data-testid="share-btn">Share</button>
+          <button data-testid="favorite-btn">Favorite</button>
+          <h3 data-testid="recipe-title">{ drink.strDrink }</h3>
+          <img
+            data-testid="recipe-photo"
+            src={ drink.strDrinkThumb }
+            alt={ drink.strDrink }
+          />
+          <p data-testid="recipe-category">{ drink.strAlcoholic }</p>
+          <p data-testid="instructions">{ drink.strInstructions }</p>
+          <ul>
+            {getIngredients(drink).map((ingredient, index) => (
+              <li
+                key={ index }
+                data-testid={ `${drinkIndex}-${index}-ingredient-name-and-measure` }
+              >
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+          <button data-testid="finish-recipe-btn">Finish Recipe</button>
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/MealProgress.tsx
+++ src/pages/MealProgress.tsx
@@ -1,5 +1,70 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMealsRecipesDetails } from '../utils/SearchApi';
+
 export default function MealProgress() {
+  const { id } = useParams();
+  const [meals, setMeals] = useState<any>([]);
+
+  useEffect(() => {
+    if (!id) return;
+    const fechtsApi = async () => {
+      try {
+        const dataMeals = await fetchMealsRecipesDetails(id);
+        if (dataMeals) {
+          setMeals(dataMeals);
+        }
+      } catch (error) {
+        console.error('Erro ao buscar detalhes da refeição:', error);
+      }
+    };
+    fechtsApi();
+  }, [id]);
+
+  const getIngredients = () => {
+    if (!meals || !meals.length) return [];
+    const meal = meals[0];
+    const validIngredients: string[] = [];
+    for (let index = 1; index <= 20; index++) {
+      const ingredientKey = `strIngredient${index}`;
+      const measureKey = `strMeasure${index}`;
+      const ingredientValue = meal[ingredientKey];
+      const measureValue = meal[measureKey];
+      if (ingredientValue && ingredientValue !== null) {
+        const combinedValue = `${measureValue} ${ingredientValue}`;
+        validIngredients.push(combinedValue);
+      }
+    }
+    return validIngredients;
+  };
+
   return (
-    <div>MealProgress</div>
+    <div>
+      {meals.map((meal: any, mealIndex: any) => (
+        <div key={ mealIndex }>
+          <img
+            src={ meal.strMealThumb }
+            alt={ meal.strMeal }
+            data-testid="recipe-photo"
+          />
+          <h1 data-testid="recipe-title">{meal.strMeal}</h1>
+          <button data-testid="share-btn">Share</button>
+          <button data-testid="favorite-btn">Favorite</button>
+          <h2 data-testid="recipe-category">{meal.strCategory}</h2>
+
+          {getIngredients().map((ingredient, index) => (
+            <li
+              key={ index }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {ingredient}
+            </li>
+          ))}
+
+          <p data-testid="instructions">{meal.strInstructions}</p>
+          <button data-testid="finish-recipe-btn">Finish Recipe</button>
+        </div>
+      ))}
+    </div>
   );
 }
